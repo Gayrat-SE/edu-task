a76fcb4ddeaa9229f0b1890953d0d5a5fd238fc1
Begzod Rasulev <begzod_rasulev@mail.ru>
2022-07-08 11:54:28 +0500

    new

--- main/api/v1/user/serializers.py
+++ main/api/v1/user/serializers.py
@@ -3,6 +3,7 @@ from user.models import User, Student, Teacher, Admin, StudentGroup
 from rest_framework.response import Response
 
 class StudentCreateSerializer(serializers.ModelSerializer):
+
     username = serializers.CharField(source='user.username')
     password = serializers.CharField(write_only = True, source='user.password')
     gender = serializers.CharField(source = 'user.gender')
@@ -12,8 +13,9 @@ class StudentCreateSerializer(serializers.ModelSerializer):
     email = serializers.CharField(source = 'user.email')
     phone = serializers.CharField(source = 'user.phone')
     name = serializers.CharField(source = 'studentgroup.name')
+
     class Meta:
-        model = StudentGroup
+        model = Student
         fields = ('username', 'password', 'gender', 'birthday', 'first_name', 'last_name', 'email', 'phone','name')
 
     def create(self, validated_data):
@@ -33,6 +35,54 @@ class StudentCreateSerializer(serializers.ModelSerializer):
         group.student.add(student.id)
         group.save()
         return student
+    
+class StudentUpdateSerializer(serializers.ModelSerializer):
+
+    username = serializers.CharField(source='user.username')
+    password = serializers.CharField(write_only = True, source='user.password')
+    gender = serializers.CharField(source = 'user.gender')
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source = 'user.last_name')
+    email = serializers.CharField(source = 'user.email')
+    phone = serializers.CharField(source = 'user.phone')
+    birthday = serializers.DateField(source = 'user.birthday')
+    
+    class Meta:
+        model = Student
+        fields = ('username', 'password', 'gender', 'birthday', 'first_name', 'last_name', 'email', 'phone')
+
+    def update(self, instance, validated_data):
+        user = validated_data.pop('user')
+        if user.get('username'):
+            instance.user.username = user['username']
+            instance.user.save()
+        if user.get('password'):
+            instance.user.set_password(user['password'])
+            instance.user.save()
+        if user.get('gender'):
+            instance.user.gender = user['gender']
+            instance.user.save()
+        if user.get('birthday'):
+            instance.user.birthday = user['birthday']
+            instance.user.save()
+        if user.get('first_name'):
+            instance.user.firts_name = user['first_name']
+            instance.user.save()
+        if user.get('last_name'):
+            instance.user.last_name = user['last_name']
+            instance.user.save()
+        if user.get('email'):
+            instance.user.email = user['email']
+            instance.user.save()
+        if user.get('phone'):
+            instance.user.phone = user['phone']
+            instance.user.save()
+        return instance
+
+
+
+
+
 class StudentListSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -84,4 +134,33 @@ class TeacherCreateSerializer(serializers.ModelSerializer):
         teacher.save()
 
         return teacher
+
+    def update(self, instance, validated_data):
+        user = validated_data.pop('user')
+        if user.get('username'):
+            instance.user.username = user['username']
+            instance.user.save()
+        if user.get('password'):
+            instance.user.set_password(user['password'])
+            instance.user.save()
+        if user.get('gender'):
+            instance.user.gender = user['gender']
+            instance.user.save()
+        if user.get('birthday'):
+            instance.user.birthday = user['birthday']
+            instance.user.save()
+        if user.get('first_name'):
+            instance.user.firts_name = user['first_name']
+            instance.user.save()
+        if user.get('last_name'):
+            instance.user.last_name = user['last_name']
+            instance.user.save()
+        if user.get('email'):
+            instance.user.email = user['email']
+            instance.user.save()
+        if user.get('phone'):
+            instance.user.phone = user['phone']
+            instance.user.save()
+        return instance
+
 
