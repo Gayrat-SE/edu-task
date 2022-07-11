7967ac76edc999b82f0ebda25b992a6b3257b8c2
Begzod Rasulev <begzod_rasulev@mail.ru>
2022-07-11 21:38:09 +0500

    new

--- main/main/urls.py
+++ main/main/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include("user.urls")),
     path('api/', include('api.urls')),
+    path('course/', include("courses.urls"))
 ]
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
