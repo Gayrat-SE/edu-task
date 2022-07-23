6ca7b77f7a1e8a4bd6aac8138eb038e2f29cc773
Begzod Rasulev <begzod_rasulev@mail.ru>
2022-07-24 02:54:25 +0500

    new

--- main/api/v1/lesson/views.py
+++ main/api/v1/lesson/views.py
@@ -1,5 +1,5 @@
-from urllib import request
 from lesson.models import Event
+from user.models import StudentGroup
 from .serializers import (
     CreateEventSerializer,
     GetEventsSerializer
@@ -16,9 +16,17 @@ class CreateEvent(CreateAPIView):
 
 
 
-class ListLesson(ListAPIView):
+class ListLessonTeacher(ListAPIView):
     serializer_class = GetEventsSerializer
     def get_queryset(self):
         teacher_lesson = Event.objects.filter(teacher_id = self.request.user.teacher.id,
             start_date__gte = self.request.GET.get('start_date'), end_date__lte = self.request.GET.get('end_date'))
         return teacher_lesson
+
+class ListLessonStudent(ListAPIView):
+    serializer_class = GetEventsSerializer
+    def get_queryset(self):
+        group = StudentGroup.objects.filter(student = self.request.user.student)[0]
+        student_lesson = Event.objects.filter(groups = group,
+            start_date__gte = self.request.GET.get('start_date'), end_date__lte = self.request.GET.get('end_date'))
+        return student_lesson
